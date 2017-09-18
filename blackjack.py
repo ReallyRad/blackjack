224573ee6ae30197a20dac42dcde5b85b20f589d
LevanBorchkhadze <32057828+LevanBorchkhadze@users.noreply.github.com>
2017-09-18 15:18:12 +0200

    change according to pep8

--- blackjack.py
+++ blackjack.py
@@ -1,26 +1,26 @@
-#TODO reformat file to match PEP8 recommendations
+# TODO reformat file to match PEP8 recommendations
 
 from game import *
-#we start by creating our two players, the player and the house
+# we start by creating our two players, the player and the house
 player = Player(Hand([]), 80, "John")
 house = Player(Hand([]), 9999999999, "House")
 
-while True: #loop that allows us to play several times
-    #reset the deck
+while True:  # loop that allows us to play several times
+    # reset the deck
     deck = Deck()
 
-    #reset both player's and house's hands
+    # reset both player's and house's hands
     player.hand = Hand([])
     house.hand = Hand([])
 
-    #deal one card from the deck to the player's hand
+    # deal one card from the deck to the player's hand
     deck.deal(player)
 
-    #after seeing his card, the player makes a bet
+    # after seeing his card, the player makes a bet
     bet = int(input("how much do you want to bet?"))
     player.bet(bet)
 
-    while True: #loop where the player draws new cards
+    while True:  # loop where the player draws new cards
         deck.deal(player)
         if player.hand.getValue() >= 21:
             break
@@ -33,11 +33,11 @@ while True: #loop that allows us to play several times
         elif answer == "n":
             break
 
-    while player.hand.getValue() <= 21: #loop where the house draws new cards to match the player's
+    while player.hand.getValue() <= 21:  # loop where the house draws new cards to match the player's
         deck.deal(house)
-        if house.hand.getValue() > player.hand.getValue() and house.hand.getValue()>=17:
+        if house.hand.getValue() > player.hand.getValue() and house.hand.getValue() >= 17:
             break
-    print ("house hand is worth", house.hand.getValue())
+    print("house hand is worth", house.hand.getValue())
 
     if player.hand.getValue() > 21:
         print("player went over 21. house wins\n")
@@ -58,4 +58,3 @@ while True: #loop that allows us to play several times
         player.pot = 0
 
     player.display_chips()
-
